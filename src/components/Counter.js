d841aa4daea21fdbe5347aca29bc9c79912f2ee9
ssonko94 <ssonkojimmy@gmail.com>
2021-06-06 05:35:26 +0300

    render the counter conditionally depending on the value of show counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,6 +8,7 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const decrementHandler = () => {
     dispatch({
@@ -27,12 +28,16 @@ const Counter = () => {
     });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({
+      type: "toggle",
+    });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
